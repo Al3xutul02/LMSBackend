5f4c7f554ea88fbe1d953d602655ab6dace70557
voicanelena <168528814+voicanelena@users.noreply.github.com>
2026-04-17 16:30:41 +0300

    Added an enum for reservation status

---
+++ Repository/Enums/Types/ReservationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Repository.Enums.Types
+{
+    public enum ReservationStatus
+    {
+        Pending,    
+        Approved,   
+        Rejected,  
+        Collected,  
+        Cancelled  
+    }
+}
