2364af8dfd239e5e023605dab9a76e8e783ab602
voicanelena <168528814+voicanelena@users.noreply.github.com>
2026-04-28 19:45:38 +0300

    Started librarian page

--- BusinessLogic/Services/LoanService.cs
+++ BusinessLogic/Services/LoanService.cs
@@ -4,17 +4,52 @@ using BusinessLogic.Services.Abstract;
 using BusinessLogic.Services.Generic;
 using Repository.Repositories.Abstract;
 using Repository.Tables;
+using Repository.Enums.Types;
+using Repository.Enums.Behaviors;
 
 namespace BusinessLogic.Services
 {
-    /// <summary>
-    /// The implementation of the <see cref="ILoanService"/> interface
-    /// </summary>
-    /// <param name="mapper">The mapper for the DTOs and models</param>
-    /// <param name="loanRepository">The loan repository the service communicates with</param>
-    public class LoanService(IMapper mapper, ILoanRepository loanRepository)
+    public class LoanService(IMapper mapper, ILoanRepository loanRepository, IBookRepository bookRepository)
         : BaseService<Loan, LoanReadDto, LoanCreateDto, LoanUpdateDto>(mapper, loanRepository), ILoanService
     {
-        private ILoanRepository LoanRepository => (ILoanRepository)_repository;
+        private readonly IBookRepository _bookRepository = bookRepository;
+
+        public override async Task<bool> UpdateAsync(LoanUpdateDto dto)
+        {
+            if (dto.Status == LoanStatus.Returned)
+            {
+                // REZOLVARE CS1501: Adăugăm 'null' pentru parametrul 'includes' care este obligatoriu în semnătură
+                var existingLoan = await _repository.GetByIdAsync(dto.Id, IncludeBehavior.NoInclude, null);
+
+                if (existingLoan != null && existingLoan.Status != LoanStatus.Returned)
+                {
+                    if (dto.BookRelations != null)
+                    {
+                        foreach (var relation in dto.BookRelations)
+                        {
+                            // REZOLVARE CS1501: Adăugăm 'null' și aici
+                            var book = await _bookRepository.GetByIdAsync(relation.ISBN, IncludeBehavior.NoInclude, null);
+
+                            if (book != null)
+                            {
+                                book.Count += relation.Count;
+
+                                if (book.Count > 0 && book.Status == BookStatus.OutOfStock)
+                                    book.Status = BookStatus.InStock;
+
+                                // REZOLVARE CS1061: Folosim 'Update' (metoda void), NU 'UpdateAsync'
+                                _bookRepository.Update(book);
+                            }
+                        }
+                        // Salvăm modificările pentru stocul cărților
+                        await _bookRepository.SaveAsync();
+                    }
+                    return await base.UpdateAsync(dto);
+                }
+            }
+
+            // Apelăm base.UpdateAsync care se ocupă de salvarea statusului împrumutului
+            return await base.UpdateAsync(dto);
+        }
     }
 }
